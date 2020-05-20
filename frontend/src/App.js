f5c87fb3f3a909e22b40671de59e246a3a4caa11
juliagonzaga <juliagonnzaga@gmail.com>
2020-05-19 23:48:45 -0300

    Testando contador e testando estados em React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Header from './Header';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let counter = useState(0);
+
+function Increment(){
+  counter++;
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Aprimorando conhecimentos na Semana OmniStack!</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <header>Contador: {counter}</header>
+      <button onClick={Increment}>Incrementar</button>
+  </div>
+    
+    
   );
 }
 
